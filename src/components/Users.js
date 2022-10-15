79093f6df8317f15164666ca662bf1ad47b9c6ae
Harun HATİBOĞLU <111196660+Harhat18@users.noreply.github.com>
2022-10-15 06:22:45 +0300

    add

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,8 +5,7 @@ function Users() {
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) => setUsers(data))
+      .then((res) => setUsers(res.data))
       .catch((e) => console.log(e))
       .finally(() => setIsLoading(false));
   }, []);
